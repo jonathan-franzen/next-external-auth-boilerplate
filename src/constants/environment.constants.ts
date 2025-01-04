bb17f36d64097c6207cb8318f1a294012dce6322
Jonathan Franzen <jonathan.franzen@gigger.se>
2025-01-04 16:04:20 +0100

    Update README

--- src/constants/environment.constants.ts
+++ src/constants/environment.constants.ts
@@ -3,8 +3,5 @@ const env: NodeJS.ProcessEnv = process.env;
 // App
 export const APP_ENV: string = (env.APP_ENV as string) || 'dev';
 
-// Public
-export const PUBLIC_FRONTEND_URL = env.NEXT_PUBLIC_FRONTEND_URL as string;
-
 // Private
 export const BACKEND_URL = env.BACKEND_URL as string;
